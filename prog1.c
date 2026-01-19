e7f2737fc278a806af272c51a78e05c4f753efb7
Tiara Baruela <baruelat@flip4.engr.oregonstate.edu>
2026-01-18 17:40:29 -0800

    Completed the Assignment

--- prog1.c
+++ prog1.c
@@ -1,4 +1,6 @@
 #include <stdio.h> // Necessary for printf()
+#include <stdlib.h> // provide rand(), srand()
+#include <time.h> // provides time()
 
 // TODO Other includes
 
@@ -8,4 +10,20 @@ int main() {
 	// is executed, it will automatically start here.
 	
 	// TODO Complete the program
+	srand(time(NULL)); 
+	
+	//random number 0 to 10
+	int myrand = rand()%11;
+
+	if (myrand <= 4){
+		printf("Eat more beef, kick less cats \n");
+
+	} else if (myrand <= 9){
+		printf("FRODO LIVES \n");
+	} else {
+		printf("Larn is the best roguelike \n");
+	}
+
+	printf("The random number was: %d \n", myrand);
+
 }
